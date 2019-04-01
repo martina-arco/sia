634317bbb758e0cc3f851bc0a36e4428aebb1026
Martina Arco <marco@itba.edu.ar>
2019-04-01 00:17:24 -0300

    greedy works with heuristic

--- src/ar/edu/itba/sia/gps/model/LinearDistanceHeuristic.java
+++ src/ar/edu/itba/sia/gps/model/LinearDistanceHeuristic.java
@@ -3,6 +3,10 @@ package ar.edu.itba.sia.gps.model;
 import ar.edu.itba.sia.gps.api.Heuristic;
 import ar.edu.itba.sia.gps.api.State;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LinearDistanceHeuristic implements Heuristic {
 
     public LinearDistanceHeuristic() {
@@ -10,6 +14,26 @@ public class LinearDistanceHeuristic implements Heuristic {
 
     @Override
     public Integer getValue(State state) {
-        return null;
+        StateImpl stateImplementation = (StateImpl) state;
+        Map<Point, Circle> circles = stateImplementation.getCircles();
+        Map<String, Point> circlePointMap = new HashMap<>();
+        Integer result = 0;
+
+        for (Map.Entry<Point, Circle> circleEntry:circles.entrySet()) {
+            circlePointMap.put(circleEntry.getValue().getColor(), circleEntry.getKey());
+        }
+
+        for (Map.Entry<Point, Square> squareEntry : stateImplementation.getSquares().entrySet()) {
+            Point squarePosition = squareEntry.getKey();
+
+            Point circlePosition = circlePointMap.get(squareEntry.getValue().getColor());
+
+            Double value = Math.sqrt(Math.pow(circlePosition.getX() - squarePosition.getX(), 2) +
+                    Math.pow(circlePosition.getY() - squarePosition.getY(),2));
+
+            result += value.intValue();
+        }
+
+        return result;
     }
 }
