3594509b3c73c92acee2ccb6a9c13a6139f1cd9b
Martina Arco <marco@itba.edu.ar>
2019-05-12 14:59:43 -0300

    nicer graphs

---
+++ tp2/plot_terrain.m
@@ -0,0 +1,2 @@
+function plot_terrain(X, Y, Z, grid_title)  xq = linspace(min(X), max(X));  yq = linspace(min(Y), max(Y));  [x,y] = meshgrid(xq, yq);  z_plot = griddata(X, Y, Z, x, y);  figure;  surf(x, y, z_plot);  title(grid_title, 'fontsize', 20);  xlabel('x');  ylabel('y');  zlabel('z');  grid on;
+endfunction
--- tp2/run.m
+++ tp2/run.m
@@ -1,1 +1,1 @@
-epochNumber = 1000batchSize = 32learnPercentage = 0.9learningRate = 0.01maxError = 0.001backpropagation(XE, S, epochNumber, batchSize, learnPercentage, learningRate, maxError)
+max_epochs = 100max_error = 0.001batch_size = 32learn_percentage = 0.9learning_rate = 0.01error_color = 'r';rate_color = 'g';backpropagation(XE, S, max_epochs, batch_size, learn_percentage, learning_rate, max_error, error_color, rate_color)
