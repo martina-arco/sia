b6ef9de38d0bb3d3a07d88ab6c1e3071eeb5dbcf
Martina Arco <marco@itba.edu.ar>
2019-05-12 17:11:55 -0300

    Auto stash before merge of "master" and "origin/master"

--- tp2/backpropagation.m
+++ tp2/backpropagation.m
@@ -1,5 +1,5 @@
 %Algoritmo de Entrenamiento de BACKPROPAGATION para Redes Neuronales
-function result = backpropagation(X, S, max_epochs, batch_size, learn_percentage, rate, dmse, error_color, rate_color)
+function result = backpropagation(X, S, max_epochs, batch_size, learn_percentage, rate, dmse, error_color, rate_color, structure)
 
   mse = Inf;                  %Asumiendo Pesos Iniciales Malos
   epoch = 0;                  %Rango de valores iniciales [-1 1]
@@ -20,25 +20,23 @@ function result = backpropagation(X, S, max_epochs, batch_size, learn_percentage
   %X_train = (X_train - X_mean) ./ X_std;
   %S_train = (S_train - S_mean) ./ S_std;
 
-  depth = 3;                  %Profundidad de la red (nro capas)
+  [aux, depth] = size(structure);                  %Profundidad de la red (nro capas)
   
   %Inicializar Matriz de Pesos para cada capa en el rango [-1,1]
   W = cell(depth-1, 1);
-  W{1} = normrnd(0, 1, [50, 2]) / sqrt(2);
-  W{2} = normrnd(0, 1, [1, 50]) / sqrt(50);
-
   B = cell(depth-1, 1);
-  B{1} = zeros(50, 1);
-  B{2} = zeros(1, 1);
+  
+  for i= 1 : depth-1;
+    W{i} = normrnd(0, 1, [structure(i+1), structure(i)]) / sqrt(structure(i));
+    B{i} = zeros(structure(i+1), 1);
+  end
 
   %Inicializar Matrices de los delta-pesos de ajuste
   dW = cell(depth-1, 1);               %Pre-alocacion de los delta pesos
   dB = cell(depth-1, 1);
 
-  %Inicializar los campos locales inducidos 'v'
+  %Inicializar los campos locales inducidos 'V'
   V = cell(depth, 1);
-  V{2} = ones(3,1);
-  V{3} = ones(1,1); 
 
   %Inicializar las salidas 'y' de cada capa
   y = cell(depth-1, 1);             %Pre-alocacion de las salidas locales
@@ -54,8 +52,8 @@ function result = backpropagation(X, S, max_epochs, batch_size, learn_percentage
       end
 
       for m = 1:depth-1
-            dW{m} = zeros(size(W{m}));
-            dB{m} = zeros(size(B{m}));
+        dW{m} = zeros(size(W{m}));
+        dB{m} = zeros(size(B{m}));
       end
       
       for p = p : batch_size + p
@@ -106,20 +104,24 @@ endfunction
 function plot_error(epoch, error, error_color)
   hold on
   figure(1);
-  semilogy(epoch, error, 'ro', 'Color', error_color)
+  semilogy(epoch, error, 'Color', error_color);
   title('Error', 'fontsize', 20);
-  xlabel('Epochs', 'fontsize', 11);
-  ylabel('Error', 'fontsize', 11);
+  xlabel('Epochs');
+  ylabel('Error');
+  set(gca,'FontSize',20)
+  drawnow
   hold off
 endfunction
 
 function plot_rate(epoch, rate, rate_color)
   hold on
   figure(2);
-  plot(epoch, rate, 'ro', 'Color', rate_color)
+  plot(epoch, rate, 'ro', 'Color', rate_color);
   title('Learning rate', 'fontsize', 20);
-  xlabel('Epochs', 'fontsize', 11);
-  ylabel('Rate', 'fontsize', 11);
+  xlabel('Epochs');
+  ylabel('Rate');
+  set(gca,'FontSize',20)
+  drawnow
   hold off
 endfunction
 
--- tp2/run.m
+++ tp2/run.m
@@ -3,7 +3,8 @@ max_error = 0.0000001
 batch_size = 4
 learn_percentage = 1
 learning_rate = 0.03
+structure = [2, 50, 1]
 error_color = 'm';
 rate_color = 'g';
 
-backpropagation(XE, S, max_epochs, batch_size, learn_percentage, learning_rate, max_error, error_color, rate_color)
+backpropagation(XE, S, max_epochs, batch_size, learn_percentage, learning_rate, max_error, error_color, rate_color, structure)
